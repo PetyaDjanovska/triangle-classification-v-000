4765d1fa3850fa92677c67fc35e2402c22cb9cbc
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-29 14:28:11 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,14 @@
 class Triangle
   # write code here
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+    rescue ExceptionName
+
+    end
+    end
 end
 
 class TriangleError < StandardError

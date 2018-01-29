12b660ab87edf7760b7f7b64c812c0bffcf6b38a
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-29 15:25:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,10 +11,10 @@ class Triangle
   def kind
     if a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || c + b <= a
       begin
-      raise TriangleError
+        raise TriangleError
       rescue TriangleError => error
-      puts error.message
-    end
+        puts error.message
+      end
     else
       if a == b && b == c
         :equilateral
